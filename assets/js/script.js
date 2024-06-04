4320245e4df5c7909d5d8dc47654b2b6d46110d2
Lindsay Terry <lindsaytee66@gmail.com>
2024-06-04 14:36:47 -0600

    Completed styling and data collection for the current city current weather display

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ function getLatLon() {
     })
     .then(function(data) {
         getCurrentWeather(data);
-        getForecast(data);
+        // getForecast(data);
     })
     .catch(function(error) {
         console.log('Error');
@@ -23,33 +23,74 @@ function getLatLon() {
 
 function getCurrentWeather(data) {
     const currentDate = dayjs();
-    const weatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${apiKey}`;
+    const weatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${apiKey}&units=imperial`;
     
     fetch(weatherApi) 
     .then(function(response) {
         return response.json();
     })
     .then(function(weather) {
-        console.log(weather);
+        // console.log(weather);
+        displayCurrentWeather(weather);
     })
     .catch(function(error) {
         console.log('Error');
     })
 }
 
-function getForecast(data) {
-    const forecastApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&cnt=5&appid=${apiKey}`;
+// function getForecast(data) {
+//     const forecastApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&cnt=5&appid=${apiKey}&units=imperial`;
 
-    fetch(forecastApi)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(forecast) {
-        console.log(forecast);
-    })
-    .catch(function(error) {
-        console.log('Error');
-    })
+//     fetch(forecastApi)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(forecast) {
+//         console.log(forecast);
+//     })
+//     .catch(function(error) {
+//         console.log('Error');
+//     })
+// }
+
+function displayCurrentWeather(weather) {
+    const currentWeather = document.getElementById('current-weather');
+    currentWeather.innerHTML = '';
+
+    //border around current weather for user-entered city
+    currentWeather.setAttribute('class', 'border border-dark p-2 m-2');
+    const currentWeatherDiv = document.createElement('div');
+    const nameRow = document.createElement('div');
+
+    const currentCity = document.createElement('h3');
+    const weatherIcon = document.createElement('img');
+    const currentStats = document.createElement('div');
+
+    const tempP = document.createElement('p');
+    const windP = document.createElement('p');
+    const humidityP = document.createElement('p');
+
+    nameRow.setAttribute('class', 'd-flex align-middle');
+    currentCity.textContent = `${weather.name} (${dayjs().format('M/D/YYYY')})`;
+    currentCity.setAttribute('class', 'my-2');
+    weatherIcon.setAttribute('src', `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
+    weatherIcon.setAttribute('alt', `${weather.weather[0].main} weather icon`);
+    weatherIcon.setAttribute('style', 'height: 50px;');
+
+    tempP.textContent = `Temp: ${weather.main.temp} \u00B0F`;
+    windP.textContent = `Wind: ${weather.wind.speed} MPH`;
+    humidityP.textContent = `Humidity: ${weather.main.humidity}%`;
+
+    currentWeatherDiv.appendChild(nameRow);
+    nameRow.appendChild(currentCity);
+    nameRow.appendChild(weatherIcon);
+    currentWeatherDiv.appendChild(currentStats);
+
+    currentStats.appendChild(tempP);
+    currentStats.appendChild(windP);
+    currentStats.appendChild(humidityP);
+    currentWeather.appendChild(currentWeatherDiv);
+
 }
 
 
